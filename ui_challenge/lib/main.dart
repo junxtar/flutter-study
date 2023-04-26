c861f7bbefea647ac917e9a0363c6bc86de448a6
junxtar <aa1870088@naver.com>
2023-04-26 19:54:23 +0900

    button section

--- ui_challenge/lib/main.dart
+++ ui_challenge/lib/main.dart
@@ -10,11 +10,12 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: const Color(0xFF181818),
-      body: Padding(
+      home: Scaffold(
+        backgroundColor: const Color(0xFF181818),
+        body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const SizedBox(
                 height: 20,
@@ -43,9 +44,54 @@ class App extends StatelessWidget {
                     ],
                   )
                 ],
-              )
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Text(
+                "Total Balance",
+                style: TextStyle(
+                  color: Colors.white.withOpacity(0.8),
+                  fontSize: 18,
+                ),
+              ),
+              const SizedBox(
+                height: 3,
+              ),
+              const Text(
+                "\$ 5 194 482",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Row(children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(45),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 15,
+                      horizontal: 40,
+                    ),
+                    child: Text(
+                      "Transfer",
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                )
+              ]),
             ],
-          )),
-    ));
+          ),
+        ),
+      ),
+    );
   }
 }
