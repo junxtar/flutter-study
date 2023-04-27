6935dde8e3ca65e32af008dcbba15c123d18671c
junxtar <aa1870088@naver.com>
2023-04-27 14:36:23 +0900

    build context

--- stateful_widgets/lib/main.dart
+++ stateful_widgets/lib/main.dart
@@ -12,38 +12,49 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  List<int> numbers = [];
-
   void onPressed() {
-    setState(() {
-      numbers.add(numbers.length);
-    });
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "Click Count",
-              style: TextStyle(
-                fontSize: 30,
-              ),
-            ),
-            for (var value in numbers) Text('$value'),
-            IconButton(
-              iconSize: 40,
-              onPressed: onPressed,
-              icon: const Icon(Icons.add_box_outlined),
-            ),
-          ],
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
         ),
       ),
-    ));
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              MyLargeTitle(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "My Large Title",
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge!.color,
+        // color: Theme.of(context).textTheme.titleLarge?.color,
+      ),
+    );
   }
 }
