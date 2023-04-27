f4eaf26eeefe4460a6518b6332038105f6ac771f
junxtar <aa1870088@naver.com>
2023-04-27 13:57:40 +0900

    state

---
+++ stateful_widgets/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void onPressed() {
+    counter += 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Click Count",
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+            Text(
+              "$counter",
+              style: const TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            IconButton(
+              iconSize: 40,
+              onPressed: onPressed,
+              icon: const Icon(Icons.add_box_outlined),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
