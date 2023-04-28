84cb90305afe2f57139f4b200037df005283b978
junxtar <aa1870088@naver.com>
2023-04-28 11:44:09 +0900

    wait for webtoons

--- webtoon_app/lib/services/api_service.dart
+++ webtoon_app/lib/services/api_service.dart
@@ -4,18 +4,20 @@ import 'package:http/http.dart' as http;
 import 'package:webtoon_app/models/webtoon_model.dart';
 
 class ApiService {
-  final baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = "today";
+  static const String baseUrl =
+      "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = "today";
 
-  Future<List<WebtoonModel>> getTodaysToons() async {
+  static Future<List<WebtoonModel>> getTodaysToons() async {
     List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
-      final List<dynamic> webtoons = jsonDecode(response.body);
-      for (var webtton in webtoons) {
-        webtoonInstances.add(WebtoonModel.fromJson(webtton));
+      final webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        final instance = WebtoonModel.fromJson(webtoon);
+        webtoonInstances.add(instance);
       }
       return webtoonInstances;
     }
