a0807c93941ab55dc0631c84f4e48524dcb146db
junxtar <aa1870088@naver.com>
2023-04-28 12:09:13 +0900

    future builder

--- webtoon_app/lib/screens/home_screen.dart
+++ webtoon_app/lib/screens/home_screen.dart
@@ -2,36 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:webtoon_app/models/webtoon_model.dart';
 import 'package:webtoon_app/services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  void waitForWebtoon() async {
-    webtoons = await ApiService.getTodaysToons();
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    waitForWebtoon();
-  }
+  Future<List<WebtoonModel>> webttons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
-    print(webtoons);
-    print(isLoading);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
+        elevation: 2,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
         title: const Text(
           "Today's Webtoon",
           style: TextStyle(
@@ -39,9 +22,15 @@ class _HomeScreenState extends State<HomeScreen> {
             fontWeight: FontWeight.w500,
           ),
         ),
-        elevation: 2,
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.green,
+      ),
+      body: FutureBuilder(
+        future: webttons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const Text("There is data!");
+          }
+          return const Text("Loading...");
+        },
       ),
     );
   }
